b168aec99be81d7a35f94fb5b8904965fdbd0225
Zhumabekova Akylai <jumabekovaakylai7@gmail.com>
2024-05-17 18:57:59 +0600

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import RandomNumbers from "./components/RandomNumbers/RandomNumbers";
 
-
-
 const App: React.FC = () => {
-    const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+  const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
 
-    const generateNumber = (): number[] => {
-        const numbers: number[] = [];
-        while (numbers.length < 5) {
-            const newNumber = Math.floor(Math.random() * 32) + 5;
-            if (!numbers.includes(newNumber)) {
-                numbers.push(newNumber);
-            }
-        }
-        return numbers.sort((a, b) => a - b);
-    };
+  const generateNumber = (): number[] => {
+    const numbers: number[] = [];
+    while (numbers.length < 5) {
+      const newNumber = Math.floor(Math.random() * 32) + 5;
+      if (!numbers.includes(newNumber)) {
+        numbers.push(newNumber);
+      }
+    }
+    return numbers.sort((a, b) => a - b);
+  };
 
-    const handleNewNumbers = () => {
-        setNumbers(generateNumber());
-    };
+  const handleNewNumbers = () => {
+    setNumbers(generateNumber());
+  };
 
-    return (
-        <div className="App">
-            <h1>Random Number Generator</h1>
-            <div>
-                {numbers.map((number, index) => (
-                    <RandomNumbers key={index} number={number} />
-                ))}
-            </div>
-            <button onClick={handleNewNumbers}>New numbers</button>
-        </div>
-    );
+  return (
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "50px",
+        height: "100vh",
+        backgroundColor: "aliceblue",
+      }}
+    >
+      <h1 style={{ color: "red", fontSize: "40px" }}>
+        Random Number Generator
+      </h1>
+      <div style={{ display: "flex", gap: "40px" }}>
+        {numbers.map((number, index) => (
+          <RandomNumbers key={index} number={number} />
+        ))}
+      </div>
+      <button
+        onClick={handleNewNumbers}
+        style={{
+          padding: "20px 30px",
+          borderRadius: "10px",
+          width: "400px",
+          border: "1px solid white",
+          backgroundColor: "green",
+          color: "white",
+          fontSize: "30px",
+        }}
+      >
+        New numbers
+      </button>
+    </div>
+  );
 };
-
 
 export default App;
--- src/components/RandomNumbers/RandomNumbers.tsx
+++ src/components/RandomNumbers/RandomNumbers.tsx
@@ -10,13 +10,15 @@ const RandomNumbers: React.FC<IRandomNumbers> = ({ number }) => {
       style={{
         display: "inline-block",
         margin: "10px",
-        padding: "10px",
+        padding: "30px",
         borderRadius: "50%",
-        background: "#f0f0f0",
+        background: "red",
         textAlign: "center",
+        color: "white",
         width: "50px",
         height: "50px",
         lineHeight: "50px",
+        fontSize: "30px",
       }}
     >
       {number}
