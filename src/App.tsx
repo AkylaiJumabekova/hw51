026f8949f4995e084cba0a02060bf20f3e20fa1e
Zhumabekova Akylai <jumabekovaakylai7@gmail.com>
2024-05-17 18:48:38 +0600

    added logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
+import React, { useState } from 'react';
 import RandomNumbers from "./components/RandomNumbers/RandomNumbers";
 
-function App() {
 
-  return (
-    <>
-      <div>
-        <RandomNumbers/>
-      </div>
-    </>
-  );
-  
-}
+
+const App: React.FC = () => {
+    const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+
+    const generateNumber = (): number[] => {
+        const numbers: number[] = [];
+        while (numbers.length < 5) {
+            const newNumber = Math.floor(Math.random() * 32) + 5;
+            if (!numbers.includes(newNumber)) {
+                numbers.push(newNumber);
+            }
+        }
+        return numbers.sort((a, b) => a - b);
+    };
+
+    const handleNewNumbers = () => {
+        setNumbers(generateNumber());
+    };
+
+    return (
+        <div className="App">
+            <h1>Random Number Generator</h1>
+            <div>
+                {numbers.map((number, index) => (
+                    <RandomNumbers key={index} number={number} />
+                ))}
+            </div>
+            <button onClick={handleNewNumbers}>New numbers</button>
+        </div>
+    );
+};
+
 
 export default App;
--- src/components/RandomNumbers/RandomNumbers.tsx
+++ src/components/RandomNumbers/RandomNumbers.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
-
- const RandomNumbers = () => {
-  return (
-    <div>RandomNumbers</div>
-  )
+interface IRandomNumbers {
+  number: number;
 }
 
-
+const RandomNumbers: React.FC<IRandomNumbers> = ({ number }) => {
+  return (
+    <div
+      style={{
+        display: "inline-block",
+        margin: "10px",
+        padding: "10px",
+        borderRadius: "50%",
+        background: "#f0f0f0",
+        textAlign: "center",
+        width: "50px",
+        height: "50px",
+        lineHeight: "50px",
+      }}
+    >
+      {number}
+    </div>
+  );
+};
 export default RandomNumbers;
